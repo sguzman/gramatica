1ed3bcebfe8c3623522db3cfbbbd23fa3812f28c
Salvador Guzman <guzmansalv@gmail.com>
2020-06-17 21:29:48 -0700

    added a bunch of stuff

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import sys
+from typing import Callable
 from typing import Dict
 from typing import IO
 from typing import List
 from typing import Tuple
+
+
+"""
+ Tuple[
+ int - len(Lexeme)
+ int - frequency
+ int - strength
+ int - (len(Lexeme) - 1) * frequency
+ ]
+"""
+TupTup = Tuple[int, int, int, int]
+TupFin = Tuple[str, int, int, int, int]
 
 
 # Yield successive n-sized
@@ -15,23 +28,47 @@ def chunk(alist: str, n: int) -> str:
         yield word
 
 
-def tally(lists: List[List[str]], n: int) -> Dict[str, int]:
-    count: Dict[str, int] = dict()
+def tally(
+        lists: List[List[str]],
+        n: int,
+        count: Dict[str, TupTup] = dict()
+        ) -> Dict[str, TupTup]:
 
+    temp: Dict[str, int] = dict()
     for alist in lists:
         chunky_soup = chunk(alist, n)
         for chunked in chunky_soup:
             chunked_str: str = ''.join(chunked)
-            if chunked_str not in count:
-                count[chunked_str] = 1
+            if chunked_str not in temp:
+                temp[chunked_str] = 1
             else:
-                count[chunked_str] = count[chunked_str] + 1
+                temp[chunked_str] = temp[chunked_str] + 1
 
-    return count
+    for k, v in temp.items():
+        count[k] = (len(k), v, len(k) * v, (len(k) - 1) * v)
+
+
+def drop(
+        alist: List[TupFin],
+        funcs: List[Callable[[TupFin], List[TupFin]]] = List[lambda x: x]
+        ) -> List[Tuple[str, int, int, int, int]]:
+
+    alist_filtered: List[TupFin] = []
+    for line in alist:
+        values: List[bool] = []
+
+        for f in funcs:
+            values.append(f(line))
+
+        if all(values):
+            alist_filtered.append(line)
+
+    return alist_filtered
 
 
 def top(count: Dict[str, int], n: int) -> List[Tuple[str, int]]:
     top_count: List[Tuple[str, int]] = []
+
     for k, v in count.items():
         tup: Tuple[str, int] = (k, v)
         top_count.append(tup)
@@ -54,9 +91,15 @@ def main() -> None:
         array: Lexeme = list(io.read())
         lists.append(array)
 
-    tallied: WordDict = tally(lists, 2)
-    ranked: List[Tuple[str, int]] = top(tallied, 10)
+    count: Dict[str, int] = dict()
+    for i in range(2, 20, 1):
+        tally(lists, i, count)
 
+    alist: List[Tuple[str, int, int, int, int]] = []
+    for k, v in count.items():
+        alist.append((k, v[0], v[1], v[2], v[3]))
+
+    ranked: List[Tuple[str, int]] = top(count, 10)
     for line in ranked:
         print(list(line[0]), line[1])
 
