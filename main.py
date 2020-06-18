33f026e2f6cc532618d95ea1c76758e732d15a5d
Salvador Guzman <guzmansalv@gmail.com>
2020-06-17 19:17:06 -0700

    set types for lexical stuff

--- main.py
+++ main.py
@@ -5,28 +5,35 @@ from typing import List
 from typing import Tuple
 
 
+Lexeme = List[str]
+Freq = Tuple[Lexeme, int]
+WordDict = Dict[Lexeme, int]
+
+
 # Yield successive n-sized
 # chunks from l.
-def chunk(alist: List[str], n: int):
+def chunk(alist: Lexeme, n: int) -> Lexeme:
     # looping till length l
     for i in range(0, len(alist), n):
-        yield alist[i:i + n]
+        word: str = alist[i:i+n]
+
+        yield word
 
 
-def top(count: Dict[str, int], n: int) -> List[Tuple[str, int]]:
-    top_count: List[Tuple[str, int]] = []
+def top(count: WordDict, n: int) -> List[Freq]:
+    top_count: List[Freq] = []
     for k, v in count.items():
-        tup: Tup[str, int] = (k, v)
+        tup: Freq = (k, v)
         top_count.append(tup)
 
-    sorted_list: List[Tuple[str, int]] =\
+    sorted_list: List[Freq] =\
         sorted(top_count, key=lambda t: t[1], reverse=True)
 
     return sorted_list[:n]
 
 
-def tally(lists: List[List[str]], n: int) -> Dict[str, int]:
-    count: Dict[str, int] = dict()
+def tally(lists: List[Lexeme], n: int) -> WordDict:
+    count: WordDict = dict()
 
     for alist in lists:
         chunky_soup = chunk(alist, n)
